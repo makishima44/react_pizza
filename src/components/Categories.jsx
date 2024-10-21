d3f6bfec8a6947ca679b081aeb7fa9c54a1750d7
Artur <metallist15431543@gmail.com>
2024-10-21 19:55:24 +0300

    add components PizzaBlock and Categories,  add render pizzas and categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
+
 function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = ["Все", "Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"];
+
   return (
-    <div class="categories">
+    <div className="categories">
       <ul>
-        <li class="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((value, i) => (
+          <li onClick={() => setActiveIndex(i)} className={activeIndex === i ? "active" : ""}>
+            {value}
+          </li>
+        ))}
       </ul>
     </div>
   );
