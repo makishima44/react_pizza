b39308a1cf6854a5ed9b36a1c4d2869b9e32d178
Artur <metallist15431543@gmail.com>
2024-10-28 21:16:29 +0300

    add Skeleton and React Router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import PizzaSkeleton from "../components/PizzaBlock/PizzaSkeleton";
+import PizzaBlock from "../components/PizzaBlock/PizzaBlock";
+
+const Home = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://67190fa37fc4c5ff8f4c46f2.mockapi.io/items")
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+
+        <div className="content__top">
+          <Categories />
+          <Sort />
+        </div>
+        <h2 className="content__title">Все пиццы</h2>
+        <div className="content__items">
+          {isLoading
+            ? [...new Array(6)].map((_, index) => <PizzaSkeleton key={index} />)
+            : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+        </div>
+      {/* </div> */}
+    </>
+  );
+};
+
+export default Home;
