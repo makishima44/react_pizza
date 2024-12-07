73ca372ea62ec25652f71a34e886d0a6898c935a
Artur <metallist15431543@gmail.com>
2024-12-07 20:34:38 +0300

    outlet

--- src/pages/FullPizza.jsx
+++ src/pages/FullPizza.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const FullPizza = () => {
   const [pizza, setPizza] = useState();
   const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchPizza() {
@@ -13,6 +14,7 @@ const FullPizza = () => {
         setPizza(data);
       } catch (error) {
         alert("Error");
+        navigate("/");
       }
     }
 
